a7ddea7ba0e36a1f5f1ed23960b6b193ba192c36
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-04 10:54:39 +0100

    feat adding more style with tailwind

--- src/app/views/Connexion.jsx
+++ src/app/views/Connexion.jsx
@@ -8,40 +8,36 @@ const Connexion= () => {
 
 return (
 
-    <div>
+    <div className='bg-white'>
         <h1 className='text-2xl font-bold mt-10 text-center mb-10'>Connexion</h1>
         <Formik>
         
             {formik => 
         <div className='flex justify-center' >
             <div className='flex flex-col items-center pb-5 pt-5 mb-10 w-full '>
-                <div className="w-2/3 max-w-xs">
+                <div className="w-full max-w-xs">
 
                         <Form>
                             <TextField  label="Adresse e-mail" name="email" type="email"/>
                             <TextField  label="Mot de passe" name="password" type="password"/>
 
                             <ReusableButton>Connexion</ReusableButton>
-                            <p className='m-3'>Mot de passe oublié? 
-                                <a href= "#" > Réinitialiser</a>
-                            </p>
 
-                            <p className='m-3'>Pas encore de compte Pixel Perfect? Inscrivez-vous !
-                            </p>
+                            <p className='m-3 text-center '>Mot de passe oublié? <a href= "#" className='underline' > Réinitialiser</a>
+                            </p>    
+                            
+                            <p className='m-3 text-center '>Pas encore de compte Pixel Perfect? Inscrivez-vous!</p>
+                            
                             <ReusableButton>S'inscire</ReusableButton>
 
-                            <p className='m-3'>Pour en savoir plus sur la gestion de vos données personnelles et pour exercer vos droits, consultez notre 
-                                <a href= "#" > Politique de protection des données.
-                                </a>
-                            </p>
-      
+                            <p className='m-3 text-center '>Pour en savoir plus sur la gestion de vos données personnelles et pour exercer vos droits, consultez notre <a href= "#" className='underline'  >Politique de protection des données.</a> 
+                            </p>    
+                                
                         </Form>
-                    
                 </div>
             </div>
         </div>
             }
-            
         </Formik>
     </div>
 )    
@@ -49,7 +45,3 @@ return (
 
 export default Connexion;
 
-
-{/* <button className="relative z-10 w-full h-10 flex items-center justify-center rounded-full bg-white border font-bold ">
-Connexion
-</button> */}
