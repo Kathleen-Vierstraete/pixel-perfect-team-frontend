b77f502645a17561054c05ebf1a3bdc9765e1bf0
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-04 11:37:40 +0100

    feat adding checkboxes with tailwind

--- src/app/views/CreateAccount.jsx
+++ src/app/views/CreateAccount.jsx
@@ -28,6 +28,21 @@ const CreateAccount= () => {
 
                             <TextField  label="Confirmation de mot de passe" name="passwordconfirm" type="password"/>
 
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> J'accepte de recevoir les promotions de Pixel Perfect</label>
+                            </div>
+
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 rounded focus:ring-red-500"/>
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Je m'inscris à la newsletter</label>
+                            </div>
+
+                            <div class="flex items-center">
+                                <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 "/>
+                                <label for="link-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">En cliquant sur Créer mon compte, je certifie avoir lu et accepté nosthe <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Conditions d’Utilisations et notre Politique de protections de données</a>.</label>
+                            </div>
+
                             <ReusableButton>Créer mon compte</ReusableButton>
                         </Form>
                 </div>
