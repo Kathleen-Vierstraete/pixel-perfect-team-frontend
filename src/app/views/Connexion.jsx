83be87dffbdc3af9fc9a316c92a4bbfada051a6a
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-04 15:48:53 +0100

    feat login adding yup axios and alert messages, wip

--- src/app/views/Connexion.jsx
+++ src/app/views/Connexion.jsx
@@ -1,64 +1,84 @@
-import React, {useState} from 'react';
-import {Formik, Form} from 'formik'; 
-import  TextField  from './../components/Connexion/TextField'
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import TextField from './../components/Connexion/TextField';
 import ReusableButton from '../components/Connexion/ReusableButton';
+import axios from 'axios';
 import * as Yup from 'yup';
 
-const Connexion= () => {
+const Connexion = () => {
+  const validate = Yup.object({
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Required'),
+    password: Yup.string().required('Required'),
+  });
 
-    const validate = Yup.object({
-        email: Yup.string()
-                    .email("Invalid email")
-                    .required("Required"),
-        password: Yup.string()
-                    .required("Required"),
-    })
-      
-return (
+  const [showAlert, setShowAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
+  const onSubmit = (values) => {
+    axios.post('http://localhost:8000/api/login_check', values)
+      .then((response) => {
+        console.log('Login successful:', response.data);
+        setShowAlert(true);
+      })
+      .catch((error) => {
+        console.error('Login error:', error);
+        setErrorMessage(error.message);
+      });
+  };
+
+  const Alert = ({ message }) => {
+    return (
+      <div className="alert alert-success font-bold">
+        {message}
+      </div>
+    );
+  };
+
+  return (
     <div className='bg-white'>
-        <h1 className='text-2xl font-bold mt-10 text-center mb-10'>Connexion</h1>
-        <Formik
-            initialValues = {{
-            email: '',
-            password: '',
-            }}
-            validationSchema={validate}
-            onSubmit={values => {
-                console.log(values)
-            }}
-         >
-        
-            {formik => 
-        <div className='flex justify-center' >
-            <div className='flex flex-col items-center pb-5 pt-5 mb-10 w-full '>
-                <div className="w-full max-w-xs">
+      <h1 className='text-2xl font-bold mt-10 text-center mb-10'>Connexion</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validate}
+        onSubmit={onSubmit}
+      >
+        {formik => (
+          <div className='flex justify-center'>
+            <div className='flex flex-col items-center pb-5 pt-5 mb-10 w-full'>
+              <div className="w-full max-w-xs">
+                <Form>
+                  <TextField label="Adresse e-mail" name="email" type="email" />
+                  <TextField label="Mot de passe" name="password" type="password" />
 
-                        <Form>
-                            <TextField  label="Adresse e-mail" name="email" type="email"/>
-                            <TextField  label="Mot de passe" name="password" type="password" />
+                  <ReusableButton type="submit">Connexion</ReusableButton>
 
-                            <ReusableButton type="submit">Connexion</ReusableButton>
+                  <p className='m-3 text-center'>
+                    Mot de passe oublié?{' '}
+                    <a href="#" className='underline'>Réinitialiser</a>
+                  </p>
+                  <p className='m-3 text-center'>
+                    Pas encore de compte Pixel Perfect? Inscrivez-vous!
+                  </p>
+                  <ReusableButton>S'inscire</ReusableButton>
 
-                            <p className='m-3 text-center '>Mot de passe oublié? <a href= "#" className='underline' > Réinitialiser</a>
-                            </p>    
-                            
-                            <p className='m-3 text-center '>Pas encore de compte Pixel Perfect? Inscrivez-vous!</p>
-                            
-                            <ReusableButton>S'inscire</ReusableButton>
+                  <p className='m-3 text-center'>
+                    Pour en savoir plus sur la gestion de vos données personnelles
+                    et pour exercer vos droits, consultez notre{' '}
+                    <a href="#" className='underline'>Politique de protection des données.</a>
+                  </p>
 
-                            <p className='m-3 text-center '>Pour en savoir plus sur la gestion de vos données personnelles et pour exercer vos droits, consultez notre <a href= "#" className='underline'>Politique de protection des données.</a> 
-                            </p>    
-                                
-                        </Form>
-                </div>
+                  {showAlert && <Alert message="Connexion réussie !" />}
+                  {errorMessage && <Alert message={errorMessage} />}
+                </Form>
+              </div>
             </div>
-        </div>
-            }
-        </Formik>
+          </div>
+        )}
+      </Formik>
     </div>
-)    
+  );
 };
 
 export default Connexion;
-
