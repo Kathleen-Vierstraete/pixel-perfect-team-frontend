4347f601d270975d0c1db988e13e72875adb21f4
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-06 15:02:33 +0100

    feat: connexion & create account, checks before merge

--- src/app/components/Connexion/ReusableButton.jsx
+++ src/app/components/Connexion/ReusableButton.jsx
@@ -4,8 +4,6 @@ const ReusableButton = ({ children, type = 'button' }) => {
   return (
     <button className="relative z-10 w-full h-10 flex items-center justify-center rounded-full bg-white border font-bold mx-0.5 " type={type}>
         {children}
-   
-      
     </button>
   );
 };
