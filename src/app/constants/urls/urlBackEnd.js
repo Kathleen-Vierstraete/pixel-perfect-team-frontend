30aae51ba72f3ac43bf64d6fe54f933763c45882
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-07 10:21:50 +0100

    fix : routes for create account

--- src/app/constants/urls/urlBackEnd.js
+++ src/app/constants/urls/urlBackEnd.js
@@ -1,8 +1,4 @@
 export const URL_BACK_AUTHENTICATE = '/authenticate';
 export const URL_BACK_PRODUCTS_BY_TAGS = '/products/tags'
 export const URL_BACK_LOGIN_CHECK = "/login_check"
-<<<<<<< HEAD
-export const URL_BACK_CREATE_ACCOUNT = "/user/add"
-=======
 export const URL_BACK_CREATE_ACCOUNT = "/users"
->>>>>>> feat/login
