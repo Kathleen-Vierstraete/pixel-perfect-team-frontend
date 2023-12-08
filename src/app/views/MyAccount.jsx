6fb95af715a143d6dc13370cba5b4e2e1f405ee4
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-08 14:13:43 +0100

    feat completing first template, wip

--- src/app/views/MyAccount.jsx
+++ src/app/views/MyAccount.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { useSelector } from "react-redux";
-import { selectUser} from "../redux-store/authenticationSlice";
+import { selectUser, signOut } from "../redux-store/authenticationSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { URL_CONNEXION } from "../constants/urls/urlFrontEnd";
+
 
 
 
 const MyAccount = () => {
 
-
     const user = useSelector(selectUser);
+    const navigate = useNavigate()
+
+
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(signOut());
+        navigate(URL_CONNEXION);
+    };
 
   return (
-    <div className='mx-auto'>
-  <div className="flex justify-center">
-    <h5 className="mb-10 font-SilkScreen">Bonjour {user.userFirstName}</h5>
-  </div>
+    <div className='mx-auto mb-32'>
+        <div className="flex justify-center ">
+            <h5 className="mb-40 font-SilkScreen">Bonjour {user.userFirstName}</h5>
+        </div>
 
   <div className="grid grid-cols-2 lg:grid-cols-2 place-items-center">
-    <div className="grid-col md:grid-cols-1 lg:grid-col-2 mb-5">
-      <div className="mb-6">
+    <div className="grid-col md:grid-cols-1 lg:grid-col-2">
+      <div className="mb-14">
         <h6 className="mb-1 flex justify-center font-bold md:justify-start ">
           Mon profil
         </h6>
@@ -38,8 +51,8 @@ const MyAccount = () => {
       </div>
     </div>
 
-    <div className="grid-col md:grid-cols-1 lg:grid-col-2 mb-5">
-      <div className="mb-6">
+    <div className="grid-col md:grid-cols-1 lg:grid-col-2">
+      <div className="mb-14">
         <h6 className="mb-1 flex justify-center font-bold md:justify-start">
           Mes commandes
         </h6>
@@ -60,6 +73,13 @@ const MyAccount = () => {
       </div>
     </div>
   </div>
+
+
+        <div className="flex justify-center mt-20 ">
+            <button className="block w-1/7 text-left py-2 px-4 rounded-lg hover:bg-primary-light" onClick={handleLogout}>
+            Me déconnecter
+            </button>
+        </div>
 </div>
 
 
