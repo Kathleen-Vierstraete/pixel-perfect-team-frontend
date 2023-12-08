a5ab34b8e2fbbc18b454629d947ed898acb38ec8
Jonathan <jonathan.bosman@gmail.com>
2023-12-08 11:36:02 +0100

    feat: new branch, class CategoryLinks renamed to CategoryView and moved to views, imported Spinner and ProductList

--- src/app/views/CategoryView.jsx
+++ src/app/views/CategoryView.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Spinner } from "../components/animation/Spinner";
+import ProductList from "../components/Product/ProductList";
 
-const CategoryLinks = ({}) => {
+const CategoryView = ({}) => {
   const params = useParams();
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -11,7 +13,6 @@ const CategoryLinks = ({}) => {
     axios
       .get(`http://localhost:8000/api/categories/${params.id}/products`)
       .then((res) => {
-        // console.log(res.data)
         setProducts(res.data);
         setIsLoading(false);
       })
@@ -23,19 +24,13 @@ const CategoryLinks = ({}) => {
   // TODO : gérer l'erreur en cas d'ID non trouvé en BDD
 
   return isLoading ? (
-    <p>Loading...</p>
+    <Spinner />
   ) : (
     <div>
       <div className="text-2xl underline">{products[0].category.label}</div>
-      <ul>
-        {products.map((product) => (
-          <div key={product.id}>
-            <li>{product.name}</li>
-          </div>
-        ))}
-      </ul>
+        <ProductList products={products} />
     </div>
   );
 };
 
-export default CategoryLinks;
+export default CategoryView;
