80bff3d3a820d0d6150f0db45b47a8c20571f01a
Nicolas <99329663+Novawolf276@users.noreply.github.com>
2023-12-14 14:31:32 +0100

    fix: route

--- src/app/constants/urls/urlBackEnd.js
+++ src/app/constants/urls/urlBackEnd.js
@@ -5,7 +5,9 @@ export const URL_PRODUCT_BY_ID = (id = null) => { return id === null ? '/product
 export const URL_BACK_PRODUCTS_BY_TAGS = '/products/tags'
 export const URL_BACK_LOGIN_CHECK = "/login_check"
 export const URL_BACK_CREATE_ACCOUNT = "/users"
-export const URL_BACK_PERSON = (id = null) => { return id === null ? "/persons/:id" : `/persons/${id}` };
+export const URL_BACK_PERSON = (id = null) => { return id === null ? "/users/:id" : `/users/${id}` };
 export const URL_BACK_PURCHASE = (id) => { return `/persons/${id}/purchases`};
 export const URL_BACK_ADD_ADDRESSE = (id) => {return `/persons/${id}/addresses`};
 export const URL_BACK_UPDATE_ADDRESSE = (id) => {return `/addresses/${id}`};
+
+
