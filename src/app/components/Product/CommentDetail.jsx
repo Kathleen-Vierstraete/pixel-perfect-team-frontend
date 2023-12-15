2fc8d3b1fc7151ad5677320eef90c91b7d6d8edc
Jonathan <jonathan.bosman@gmail.com>
2023-12-15 14:44:25 +0100

    feat: average score, average stars

--- src/app/components/Product/CommentDetail.jsx
+++ src/app/components/Product/CommentDetail.jsx
@@ -1,40 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiThumbsUp, FiThumbsDown } from "react-icons/fi";
-import { FaStar, FaRegStar } from "react-icons/fa";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
 const CommentDetail = ({ product }) => {
+  const average = Math.round((product.comments.map(comment => comment.rate).reduce((a, b) => a + b) / 10) * 2) / 2;
+  console.log(average, product.comments.length);
+
   return (
-    <div className="flex flex-col lg:flex-row lg:flex-wrap lg:justify-between">
-      {product.comments.map((comment, index) => (
-        <div
-          className="flex flex-col lg:w-[48%] gap-4 p-5 my-4 bg-primary text-white rounded-lg"
-          key={index}
-        >
-          <div className="flex justify-between">
-            <div className="flex items-center">
-              {[...Array(5)].map((x, i) =>
-                i+1<=comment.rate?<FaStar key={i} />:<FaRegStar />
-              )}
-            </div>
-            {/* <div>{<FiThumbsUp />.repeat(comment.rate)+"b".repeat(5-comment.rate)}</div> */}
-            <div>{comment.date}</div>
-          </div>
-
-          <div className="underline">{comment.title}</div>
-
-          <div className="text-xs lg:text-base">{comment.body}</div>
-
-          <div className="flex justify-between">
-            <div className="capitalize">{comment.person.firstName}</div>
-            <div className="flex gap-2 items-center">
-              <div>{comment.vote}</div>
-              <div><FiThumbsUp /></div>
-              <div><FiThumbsDown /></div>
-            </div>
+    <>
+      <div className="flex justify-between">
+        <div>
+          <div>Moyenne :</div>
+          <div className= "text-xl lg:text-4xl font-bold text-primary">{average}/5</div>
+          <div className="flex items-center text-lg lg:text-2xl text-secondary">
+            {[...Array(5)].map((x, i) =>
+              i + 1 <= average ? <FaStar key={i} /> : i + 0.5 <= average ? <FaStarHalfAlt key={i} /> : <FaRegStar key={i} />
+            )}
           </div>
         </div>
-      ))}
-    </div>
+        <div>
+          Detail
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row lg:flex-wrap lg:justify-between">
+        {product.comments.map((comment, index) => (
+          <div
+            className="flex flex-col lg:w-[48%] gap-4 p-5 my-4 bg-primary text-white rounded-lg"
+            key={index}
+          >
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                {[...Array(5)].map((x, i) =>
+                  i + 1 <= comment.rate ? <FaStar key={i} /> : <FaRegStar key={i} />
+                )}
+              </div>
+              <div>{comment.date}</div>
+            </div>
+
+            <div className="underline">{comment.title}</div>
+
+            <div className="text-xs lg:text-base">{comment.body}</div>
+
+            <div className="flex justify-between">
+              <div className="capitalize">{comment.person.firstName}</div>
+              <div className="flex gap-2 items-center">
+                <div>{comment.vote}</div>
+                <div><FiThumbsUp /></div>
+                <div><FiThumbsDown /></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
