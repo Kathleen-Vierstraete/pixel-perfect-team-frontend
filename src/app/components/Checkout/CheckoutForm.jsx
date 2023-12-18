9739bf1392e6bd9e8a4fdde24dc0df595e3a1a9f
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-18 11:39:57 +0100

    feat: adding more stripe feature, with confirm payment

--- src/app/components/Checkout/CheckoutForm.jsx
+++ src/app/components/Checkout/CheckoutForm.jsx
@@ -1,9 +1,44 @@
-import {PaymentElement} from '@stripe/react-stripe-js';
+import {useStripe, useElements, PaymentElement} from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const result = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "http://localhost:5173/",
+      },
+    });
+
+    if (result.error) {
+      // Show error to your customer (for example, payment details incomplete)
+      console.log(result.error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+      console.log(result);
+
+    }
+  };
+
   return (
     <div className='flex justify-center'>
-    <form className='flex flex-col justify-center text-red-600'>
+    <form onSubmit={handleSubmit} className='flex flex-col justify-center text-red-600'>
       <PaymentElement/>
       <button className="block w-1/4 py-2 text-center text-white m-auto mt-2 rounded-lg bg-primary-light hover:bg-primary-dark">Payer</button>
     </form>
