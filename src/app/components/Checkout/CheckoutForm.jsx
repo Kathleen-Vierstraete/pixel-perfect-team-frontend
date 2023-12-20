a7433fc5c83571f71e3b1a77cec0d21de9f2cb75
Eric Sergueev <eric.sergueev@gmail.com>
2023-12-20 12:29:02 +0100

    feat : add change purchase status when transaction completed

--- src/app/components/Checkout/CheckoutForm.jsx
+++ src/app/components/Checkout/CheckoutForm.jsx
@@ -68,7 +68,7 @@ const CheckoutForm = ({ token }) => {
 
     //const to be able to clear cart and redirect
     const returnHome = () => {
-      apiBackEnd.get(URL_BACK_SET_PURCHASE_STATUS(purchaseId), setHearderToken(token))
+      apiBackEnd.post(URL_BACK_SET_PURCHASE_STATUS(purchaseId),{name:"en cours"}, setHearderToken(token))
         .catch(error => {
           console.error("Error update purchase : ", error)
         })
