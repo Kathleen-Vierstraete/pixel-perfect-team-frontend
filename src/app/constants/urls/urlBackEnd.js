4c2081d831ab0b5e278a2c001d4f1d4db4902c33
Eric Sergueev <eric.sergueev@gmail.com>
2023-12-20 12:06:33 +0100

    fix : change route back person

--- src/app/constants/urls/urlBackEnd.js
+++ src/app/constants/urls/urlBackEnd.js
@@ -7,12 +7,13 @@ export const URL_BACK_COMMENTS = "comments";
 export const URL_BACK_PRODUCTS_BY_TAGS = '/products/tags'
 export const URL_BACK_LOGIN_CHECK = "/login_check"
 export const URL_BACK_CREATE_ACCOUNT = "/users"
-export const URL_BACK_PERSON = (id = null) => { return id === null ? "/users/:id" : `/users/${id}` };
-export const URL_BACK_PURCHASE = (id) => { return `/persons/${id}/purchases`};
-export const URL_BACK_ADD_ADDRESSE = (id) => {return `/persons/${id}/addresses`};
-export const URL_BACK_UPDATE_ADDRESSE = (id) => {return `/addresses/${id}`};
+export const URL_BACK_PERSON = (id = null) => { return id === null ? "/persons/:id" : `/persons/${id}` };
+export const URL_BACK_PURCHASE = (id) => { return `/persons/${id}/purchases` };
+export const URL_BACK_ADD_ADDRESSE = (id) => { return `/persons/${id}/addresses` };
+export const URL_BACK_UPDATE_ADDRESSE = (id) => { return `/addresses/${id}` };
 export const URL_BACK_ADMINISTRATORS = "/administrators"
 export const URL_BACK_PRODUCT_CREATE = "/products"
 export const URL_BACK_STRIPE = "/stripe/create"
-export const URL_BACK_PICK_BY_USER = (id) =>{return `/persons/${id}/picks`}
-export const URL_BACK_GET_PICK = (id) => {return `/picks/${id}`}
+export const URL_BACK_PICK_BY_USER = (id) => { return `/persons/${id}/picks` }
+export const URL_BACK_GET_PICK = (id) => { return `/picks/${id}` }
+export const URL_BACK_SET_PURCHASE_STATUS = (id) => { return `/purchases/${id}/status` }
